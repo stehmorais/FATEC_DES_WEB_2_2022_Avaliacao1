c4f6f3cb836658c64b60cce21e4ef75df6279064
Ester Morais <97535906+stehmorais@users.noreply.github.com>
2022-09-27 22:39:27 -0300

    ajustes finais na verificação dos dados

--- Sistema de login/dados.php
+++ Sistema de login/dados.php
@@ -5,6 +5,7 @@ session_start();
 if (!isset($_SESSION["logado"]) || $_SESSION["logado"] !== true) {
     header("location: index.php");
     exit;
+}
 
 $nome = $_POST['nome'];
 $sobrenome = $_POST['sobrenome'];
